0c579902f29a6247a3bd5913acbca6a700c90599
doppera-at <github@doppera.at>
2026-02-03 19:13:53 +0100

    fix broken path for components

--- docs/js/load-components.js
+++ docs/js/load-components.js
@@ -1,2 +1,2 @@
-$("header").first().load("../components/header.html");
-$("nav").first().load("../components/navigation.html");
+$("header").first().load("components/header.html");
+$("nav").first().load("components/navigation.html");
